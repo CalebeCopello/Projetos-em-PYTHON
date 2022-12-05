48758c843c27aa6e9aa1ff837417cf7faab8360a
CalebeCopello <calebecopello@gmail.com>
2022-12-05 10:21:57 -0300

    Update forca.py
    Maior interação com o usuário, começando a mostrar o resultado das tentativas

--- #003 Jogo Forca/forca.py
+++ #003 Jogo Forca/forca.py
@@ -1,13 +1,29 @@
 from random import randint
+#definindo variaveis
 palavras = ['agua', 'pao', 'carro', 'dado', 'fogueira']
 pal_secreta = palavras[randint(0,len(palavras) - 1)]
+tentativas = 5
+#interação com o usuário
 print(f'A palavra secreta tem {len(pal_secreta)} letras')
+#mostrando a quantidade de letras
+print('      _______')
+print('     |/      |')
+print('     |')
+print('     |')
+print('     |')
+print('     |')
+print('     |')
+print('  ___|___')
+print('')
 for c in range(0, len(pal_secreta)):
     print(f'_', end=' ')
 print('')
-while True:
+print('')
+#iniciando o loop do jogo
+while tentativas > 0:
+#pegando informação do usuário
     while True:
-        tentativa = str(input('Qual é a primeira letra da palavra?'))
+        tentativa = str(input('Digite uma letra: ')).lower()
         if tentativa.isalpha():
             if len(tentativa.strip()) > 1:
                 print('Digite apenas uma letra!')
@@ -15,4 +31,26 @@ while True:
                 break
         else:
             print('Você não digitou uma letra!')
-    break
+        print('')
+#verificando se a letra está contida na palavra
+    if tentativa in pal_secreta:
+        print(f'Parabéns a letra {tentativa} está na palavra, essa aparece na palavra {pal_secreta.count(tentativa)}')
+        for c in range(0, len(pal_secreta)):
+            if pal_secreta[c] == tentativa:
+                print(f'{tentativa.upper()}',end='')
+            else:
+                print('_',end='')
+        print('')
+    else:
+        print(f'Que pena! A letra {tentativa} não está na palavra')
+    tentativas -= 1
+
+'''print('      _______')
+print('     |/      |')
+print('     |      (_)')
+print('     |      \|/')
+print('     |       |')
+print('     |      / \\')
+print('     |')
+print('  ___|___')
+'''
