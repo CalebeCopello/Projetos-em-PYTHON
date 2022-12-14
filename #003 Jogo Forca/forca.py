eeab3f1aa37239716993c340cdaac202ab607c2d
CalebeCopello <calebecopello@gmail.com>
2022-12-14 08:23:11 -0300

    Update forca
    Finalizando a interação com o usuário

--- #003 Jogo Forca/forca.py
+++ #003 Jogo Forca/forca.py
@@ -11,7 +11,74 @@ def acento(p):
     p = p.replace('ô','o')
     p = p.replace('ú','u')
     return p
-
+#função para a forca
+def forca(n):
+    if n == 6:
+        print('      _______')
+        print('     |/      |')
+        print('     |')
+        print('     |')
+        print('     |')
+        print('     |')
+        print('     |')
+        print('  ___|___')
+        print('')
+    if n == 5:
+        print('      _______')
+        print('     |/      |')
+        print('     |      (_)')
+        print('     | ')
+        print('     |')
+        print('     |')
+        print('     |')
+        print('  ___|___')
+    if n == 4:
+        print('      _______')
+        print('     |/      |')
+        print('     |      (_)')
+        print('     |       |')
+        print('     |       |')
+        print('     |')
+        print('     |')
+        print('  ___|___')
+    if n == 3:
+        print('      _______')
+        print('     |/      |')
+        print('     |      (_)')
+        print('     |      \|')
+        print('     |       |')
+        print('     |')
+        print('     |')
+        print('  ___|___')
+    if n == 2:
+        print('      _______')
+        print('     |/      |')
+        print('     |      (_)')
+        print('     |      \|/')
+        print('     |       |')
+        print('     |')
+        print('     |')
+        print('  ___|___')
+    if n == 1:
+        print('      _______')
+        print('     |/      |')
+        print('     |      (_)')
+        print('     |      \|/')
+        print('     |       |')
+        print('     |      /')
+        print('     |')
+        print('  ___|___')
+    if n == 0:
+        print('      _______')
+        print('     |/      |')
+        print('     |      (_)')
+        print('     |      \|/')
+        print('     |       |')
+        print('     |      / \\')
+        print('     |')
+        print('  ___|___')
+        
+        
 #importando modulos
 from random import randint
 #definindo variaveis
@@ -24,17 +91,9 @@ tentativa_erro = ''
 for c in range(0, len(pal_secreta)):
     pal_dica.append('_')
 #interação com o usuário
-print(f'A palavra secreta tem {len(pal_secreta)} letras')
 #mostrando a quantidade de letras
-print('      _______')
-print('     |/      |')
-print('     |')
-print('     |')
-print('     |')
-print('     |')
-print('     |')
-print('  ___|___')
-print('')
+print(f'A palavra secreta tem {len(pal_secreta)} letras')
+forca(6)
 for c in range(0, len(pal_dica)):
     print(f'{pal_dica[c]}', end=' ')
 print('')
@@ -55,24 +114,19 @@ while tentativas > 0:
 #verificando se a letra está contida na palavra
     if tentativa in acento(pal_secreta) and tentativa.upper() not in pal_dica:
         print(f'Parabéns a letra {tentativa.upper()} está na palavra, essa letra aparece na palavra {acento(pal_secreta).count(tentativa)} vez(es)')
+        forca(tentativas)
         for c in range(0, len(pal_secreta)):
             if acento(pal_secreta[c]) == tentativa:
                 pal_dica[c] = tentativa.upper()
     elif tentativa.upper() in pal_dica or tentativa.upper() in tentativa_erro.upper():
         print(f'Você já tentou a letra {tentativa.upper()}, tente outra letra')
+        forca(tentativas)
+        print(tentativa_erro)
     elif tentativa not in acento(pal_secreta):
         print(f'Que pena! A letra {tentativa} não está na palavra')
         tentativas -= 1
         tentativa_erro = tentativa + tentativa_erro
-        if tentativas == 5:
-            print('      _______')
-            print('     |/      |')
-            print('     |      (_)')
-            print('     | ')
-            print('     |')
-            print('     |')
-            print('     |')
-            print('  ___|___')
+        forca(tentativas)
         print(tentativa_erro)
     for c in range(0, len(pal_dica)):
         print(f'{pal_dica[c]}', end=' ')
@@ -82,13 +136,3 @@ while tentativas > 0:
         break
     if tentativas == 0:
         print(f'Você perdeu\nA palavra era {pal_secreta}')
-
-'''print('      _______')
-print('     |/      |')
-print('     |      (_)')
-print('     |      \|/')
-print('     |       |')
-print('     |      / \\')
-print('     |')
-print('  ___|___')
-'''
